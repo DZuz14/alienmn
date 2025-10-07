a0815259c002d68eae4968d494f999983deeb19e
z <danielzuzevich@gmail.com>
2025-10-07 10:53:10 -0400

    Auto commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { GlowingBorderBottom } from '@/components';
 
 export default function Home() {
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { default as NavBar } from './NavBar/NavBar';
 export { default as ContactForm } from './ContactForm/ContactForm';
 export { default as StarryNightSky } from './StarryNightSky/StarryNightSky';
-export { default as GlowingText } from './GlowingText/GlowingText';
 export { default as GlowingBorderBottom } from './GlowingBorderBottom/GlowingBorderBottom';
 export { default as BackToTop } from './BackToTop/BackToTop';
 export { default as Bubbles } from './Bubbles/Bubbles';
