53f580b0bd41dcefc07eded06629aabbcfce9279
z <danielzuzevich@gmail.com>
2026-01-20 12:47:45 -0500

    Auto commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'alienmn'; // <-- your GitHub repo name
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // makes `out/` on `next build`
+  images: { unoptimized: true }, // required for static export if using next/image
+
+  // For https://<user>.github.io/<repo>/
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+
+  // Helps GitHub Pages routing for "/about/" style paths
+  trailingSlash: true,
 };
 
 export default nextConfig;
---
+++ out/_next/static/chunks/pages/_app-0586ab99261341f1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{2281:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(1437)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[355,792],()=>(n(2281),n(4609))),_N_E=_.O()}]);
